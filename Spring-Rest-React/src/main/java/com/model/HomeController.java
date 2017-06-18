880f08ba927f0bdac0d0858a80c9a8d1f93f6fce
Yamen Jeries <yamenj@gmail.com>
2017-06-18 15:35:36 +0200

    FIX: @Controller annotation must not be changed in HomeController
    A new class MyRestController with the annotation @RestController was created as a work around

--- Spring-Rest-React/src/main/java/com/model/HomeController.java
+++ Spring-Rest-React/src/main/java/com/model/HomeController.java
@@ -15,24 +15,17 @@
  */
 package com.model;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.basex.Utils;
 
 /**
  * @author Greg Turnquist
  */
 // tag::code[]
-@RestController
+@Controller
+//@RestController
 @RequestMapping("/")
 public class HomeController {
 
