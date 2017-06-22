29b72f962772a66a1ff8f6280f3eedab33a893c5
hyekyeong <hyekyeong.h@gmail.com>
2017-06-22 14:39:10 +0200

    Update filterjs.js

--- Spring-Rest-React/src/main/resources/static/filterjs.js
+++ Spring-Rest-React/src/main/resources/static/filterjs.js
@@ -1,3 +1,87 @@
+class SelectCountry extends React.Component {
+	  constructor(props) {
+	    super(props);
+	    
+	    this.state = { value: 'select'};
+	  }
+	  onChange(e) {
+	    this.setState({
+	      value: e.target.value
+	    })
+	  }
+	  render() {
+	    return (
+	      <div className="form-group">
+	      	<p>
+	      		<br/>
+	      	</p>
+	        <select value={this.state.value} onChange={this.onChange.bind(this)} className="form-control">
+	          <option value="select">Select</option>
+	          <option value="schweden">Schweden</option>
+	          <option value="deutschland">Deutschland</option>
+	        </select>
+	      </div>
+	    )
+	  }
+}
+
+class SelectCategory extends React.Component {
+	  constructor(props) {
+	    super(props);
+	    
+	    this.state = { value: 'select'};
+	  }
+	  onChange(e) {
+	    this.setState({
+	      value: e.target.value
+	    })
+	  }
+	  render() {
+	    return (
+	      <div className="form-group">
+	      	<p>
+	      		<br/>
+	      	</p>
+	        <select value={this.state.value} onChange={this.onChange.bind(this)} className="form-control">
+	          <option value="select">Select</option>
+	          <option value="gemaelde">Gemälde</option>
+	          <option value="Keramik">Keramik</option>
+	          <option value="portrait">Portrait</option>
+	        </select>
+	      </div>
+	    )
+	  }
+}
+
+class SelectArtist extends React.Component {
+	  constructor(props) {
+	    super(props);
+	    
+	    this.state = { value: 'select'};
+	  }
+	  onChange(e) {
+	    this.setState({
+	      value: e.target.value
+	    })
+	  }
+	  render() {
+	    return (
+	      <div className="form-group">
+	      	<p>
+	      		<br/>
+	      	</p>
+	        <select value={this.state.value} onChange={this.onChange.bind(this)} className="form-control">
+	        	<option value="select">Select an option</option>
+	        	<option value="a-f">A-F</option>
+	        	<option value="h-m">H-M</option>
+	        	<option value="n-t">N-T</option>
+	        	<option value="u-z">U-Z</option>
+	        </select>
+	      </div>
+	    )
+	  }
+}
+
 class SelectTest extends React.Component {
 	  constructor(props) {
 	    super(props);
@@ -26,4 +110,8 @@ class SelectTest extends React.Component {
 	  }
 }
 
+
+ReactDOM.render(<SelectCountry />, document.getElementById('selectCountry') );
+ReactDOM.render(<SelectCategory />, document.getElementById('selectCategory') );
+ReactDOM.render(<SelectArtist />, document.getElementById('selectArtist') );
 ReactDOM.render(<SelectTest />, document.getElementById('selectTest') );
