819b937e3bc104194930168d5e0eac077ed32cea
Yamen Jeries <yamenj@gmail.com>
2017-06-28 21:51:17 +0200

    Refactoring

--- Spring-Rest-React/src/main/java/com/basex/Utils.java
+++ Spring-Rest-React/src/main/java/com/basex/Utils.java
@@ -8,8 +8,8 @@ import java.util.List;
 import org.apache.commons.io.FileUtils;
 
 import com.basex.BaseXClient.Query;
-import com.dbpedia.PaintingMapper;
 import com.model.Painting;
+import com.model.PaintingMapper;
 public class Utils {
 	
 	public static List<Painting> getAllPainting() throws IOException{
