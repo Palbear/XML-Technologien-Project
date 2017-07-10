a56245d01cf4051f5a5e7a8cecbefd183a39f35f
lidiak <ruskrus@gmail.com>
2017-07-10 09:23:13 +0200

    annotated another Painting object in the smaller table with RDFa format

--- Spring-Rest-React/src/main/js/paintingList/Painting.js
+++ Spring-Rest-React/src/main/js/paintingList/Painting.js
@@ -10,10 +10,14 @@ class Painting extends React.Component{
     }
     render(){
         return(
-            <tr>
+            <tr vocab="http://schema.org/" typeof="Painting">
                 <td>{this.props.painting.recordID}</td>
-                <td>{this.props.painting.title}</td>
-                <td>{this.props.painting.artist}</td>
+                <td><span property="name">{this.props.painting.title}</span></td>
+                <td>
+                	<div property="creator" typeof="Person">
+                		<span property="name">{this.props.painting.artist}</span>
+                	</div>              	
+                </td>
                 <td>{this.props.painting.category}</td>
                 <td>
                     <ToggleImg imgurl={this.props.painting.image_link} />
