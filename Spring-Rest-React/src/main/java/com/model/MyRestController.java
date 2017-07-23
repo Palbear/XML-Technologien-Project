ac664e01a8bed4e7667c893bb7ca8646686b2a71
Yamen Jeries <yamenj@gmail.com>
2017-07-23 10:08:56 +0200

    Code Cleanup

--- Spring-Rest-React/src/main/java/com/model/MyRestController.java
+++ Spring-Rest-React/src/main/java/com/model/MyRestController.java
@@ -3,10 +3,6 @@ package com.model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author Greg Turnquist
- */
-// tag::code[]
 @RestController
 @RequestMapping("/api")
 public class MyRestController {
--- Spring-Rest-React/src/main/java/com/model/PaintingsRepository.java
+++ Spring-Rest-React/src/main/java/com/model/PaintingsRepository.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.model;
 
 import java.util.List;
@@ -21,10 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-/**
- * @author Greg Turnquist
- */
-// tag::code[]
 public interface PaintingsRepository extends JpaRepository<Painting, Long> {
 	List<Painting> findByArtistLike(@Param("name") String artist, Pageable pageable);
 	List<Painting> findByCategoryLike(@Param("category") String category, Pageable pageable);
@@ -35,4 +16,3 @@ public interface PaintingsRepository extends JpaRepository<Painting, Long> {
 	//List<Painting> findByTechnique_MaterialLike(@Param("technique_material") String technique_material, Pageable pageable);
 	Painting findByRecordIDLike(@Param("recordid") String date);
 }
-// end::code[]
